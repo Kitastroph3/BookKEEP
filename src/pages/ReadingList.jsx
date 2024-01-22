1434195c0b7d1fbe531eae79b511140755b0f3bf
Kit Danovsky <cadanovsky@gmail.com>
2024-01-21 20:46:39 -0500

    Reading list scroll x

--- src/pages/ReadingList.jsx
+++ src/pages/ReadingList.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const ReadingList = ({faves}) => {
     return (
       <>
-      <div>Reading List</div>
+        <h3 style={{textAlign:"center"}}>Reading List</h3>
       {/* IF FAVES Map is greater than 0, map through results OR display empty Reading List */}
       {faves.length > 0 ? (
-        <div>
+        <div id="flexRL">
           {/* MAP through each item in list */}
           {faves.map((fave, index) => (
-            <div key={index} className="faveContainer">
-              <div className="bookDesc">
+            <div key={index} className="faveContainer" style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+              <div className="bookDesc readingListItem">
                 <p>
                   <b>{fave.title}</b>
                 </p>
